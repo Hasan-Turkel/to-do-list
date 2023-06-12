376c52ac7b44c6c27bae376e1553a216256c3297
hasan <mmhotmail>
2023-06-12 16:07:58 +0300

    local storage commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const tBody = document.getElementsByTagName("tbody");
 const date = document.getElementsByTagName("span");
+const start = document.getElementById("start")
 
 // **********variables**********
 let year = new Date().getFullYear();
@@ -10,11 +11,33 @@ let month = new Date().getMonth() + 1;
 
 date[0].innerText = `${day}/${month}/${year}`;
 
+
+let ids = []
+
+
+
 // *********addEventListeners************
 
 tBody[0].addEventListener("click", (e) => {
   clickFunction(e);
 });
+window.addEventListener("load", () => {
+  ids = localStorage.ids.split(",") || [];
+
+  if (ids = localStorage.ids.split(",")){ for (let i=0; i<ids.length; i++){
+    let a = document.getElementById(ids[i])
+    a.innerText = "+"
+    a.style.backgroundColor = "green";
+    a.parentElement.nextElementSibling.style.color = "green";
+    console.log(ids[i]);
+  }}
+})
+start.addEventListener("click", () => {
+  localStorage.clear();
+  ids=[];
+  location.reload()
+})
+
 
 // *************functions***********
 
@@ -29,9 +52,14 @@ const done = (e) => {
   e.target.innerText = "+";
   e.target.style.backgroundColor = "green";
   e.target.parentElement.nextElementSibling.style.color = "green";
+  ids.push(e.target.id)
+  localStorage.setItem("ids", ids)
 };
 const undone = (e) => {
   e.target.innerText = "-";
   e.target.style.backgroundColor = "";
   e.target.parentElement.nextElementSibling.style.color = "";
-};
+  ids = ids.filter((id)=> id !=e.target.id);
+  localStorage.setItem("ids", ids)
+}
+
